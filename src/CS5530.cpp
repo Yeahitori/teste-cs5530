2d50d60b0d16464d043ba4e3af272ba7f2f4666f
Luís Fernando <81660835+Yeahitori@users.noreply.github.com>
2022-09-28 22:05:31 -0400

    Add files via upload

---
+++ src/CS5530.cpp
@@ -0,0 +1,347 @@
+#include <string.h>
+#include <avr/pgmspace.h>
+#include <SPI.h>
+#include "CS5530.h"
+#include "Arduino.h"
+#include <stdio.h>
+//#include <intrin.h> //Library from _nop operation. Refer to "https://docs.microsoft.com/pt-br/cpp/intrinsics/nop?view=msvc-170"
+
+CS5530::CS5530():
+_spiSettings(CS5530_DEFAULT_SPI_FREQUENCY, MSBFIRST, SPI_MODE0),
+_spi(&CS5530_DEFAULT_SPI), _ss(CS5530_DEFAULT_SS_PIN)
+
+{
+  _spi->begin();
+}
+
+int CS5530::begin()
+{
+  // setup pins
+  pinMode(_ss, OUTPUT);
+
+   digitalWrite(_ss, LOW);
+   
+   
+  // set SS high
+  digitalWrite(_ss, HIGH);
+
+  // start SPI
+  _spi->begin();
+
+  return 1;
+}
+
+void CS5530::setPin(int ss)
+{
+  _ss = ss;
+}
+
+void CS5530::setSPI(SPIClass& spi)
+{
+  _spi = &spi;
+}
+
+void CS5530::setSPIFrequency(u32 frequency)
+{
+  _spiSettings = SPISettings(frequency, MSBFIRST, SPI_MODE0);
+}
+
+/*
+
+    System reset sequence as follows: 
+
+    1 - Write a logic 1 into the RS bit of the configuration register. 
+        This will reset the calibration registers and other logic (but not the serial port).
+
+        NOTE THAT A valid reset will set the RV bit in the configuration register to a logic 1. 
+
+    2 - After writing the RS bit to a logic 1, wait 8 master clock cycles(Controler, at these case, SPI Frequency). 
+        Then write the RS bit back to logic 0.
+
+        NOTE THAT the other bits in the configuration register cannot be written on this write cycle as they are being held in reset until RS is set back to logic 0. 
+        While this involves writing an entire word into the configuration register to casue the RS bit to go to logic 0, the RV bit is a read only bit, 
+        therefore a write to the configuration register will not overwrite the RV bit. 
+
+    3 - After clearing the RS bit back to logic 0, 
+        Read the Configuration Register to check the STATE of the RV bit as this indicates that a valid reset occurred.
+
+        NOTE THAT Reading the configuration register, these action clears the RV bit back to logic 0.
+
+*/
+//Inicia o reset do CS5530 - Check OK
+bool CS5530::reset(void) {
+    int i;
+    u32 tmp;
+    
+    //Initilizing SPI port (Cont 15 clock cycles at leats), writing 1111 1111 (0XFF) at config command register
+    for(i=0;i<20;i++) {
+      writeChar(CMD_SYNC1);
+    }
+     
+    // Then, write 1111 1111 (0xFE) at SYNC0 register;
+    writeChar(CMD_SYNC0);
+    // VALIDED BY ANALYSER
+    
+    // Reseting CS5530
+    writeRegister(CMD_CONFIG_WRITE,  REG_CONFIG_RS);    
+    // Write Logical Level 1 on bit adressed at 29th position on Configuration Register at CS5530;
+
+    //A função __nop é equivalente à instrução do computador NOP
+            void __nop();                     
+
+
+    //delayMicroseconds(1);  
+    /* //At these setup, Wait 1 milli seconds// (Calculate these Delay according to selected clock of SPI.) If 2Mhz == 1 cycle == 0,0005 ms == 0,5us == 500ns.
+    According to Arduino docummentation, is better to use function "millis()", due it do not stall controller during operation, as delay do.
+    Refer to "https://www.arduino.cc/reference/pt/language/functions/time/millis/" */
+
+    writeRegister(CMD_CONFIG_WRITE, CMD_NULL);
+    //delayMicroseconds(1);
+    void __nop();
+    
+    tmp = readRegister(CMD_CONFIG_READ); 
+    //delayMicroseconds(1);
+    void __nop();
+
+    if(tmp & REG_CONFIG_RV) {
+     return true;
+    }
+
+  return false;
+}
+/*
+
+What is expected to:
+Completing the reset cycle, initializes the on-chip registers to the following states:
+
+Configuration Register:     00000000(H) (0000 0000 0000 0000 0000 0000 0000 0000 BIN)
+Offset Register:            00000000(H) (0000 0000 0000 0000 0000 0000 0000 0000 BIN)
+Gain Register               01000000(H) (0001 0000 0000 0000 0000 0000 0000 0000 BIN)
+
+Esta Operação Está validada.
+*/
+
+// Funções Operacionais:
+
+void CS5530::writeRegister(u8 reg, u32 dat) {
+ 
+    writeChar(reg);
+    writeLong(dat);
+}
+
+void CS5530::setBit(u8 reg, u32 dat) {
+    u32 tmp = 0;
+    u8 cmd = 0;
+    switch (reg)
+    {
+        case CMD_GAIN_WRITE:   cmd = CMD_GAIN_READ; break; 
+        case CMD_OFFSET_WRITE: cmd = CMD_OFFSET_READ; break;		
+        case CMD_CONFIG_WRITE: cmd = CMD_CONFIG_READ; break; 
+    }
+
+    tmp =  readRegister(cmd);
+    tmp |= dat;
+    writeChar(reg);
+    writeLong(tmp);
+}
+
+void CS5530::resetBit(u8 reg, u32 dat) {
+     u32 tmp = 0;
+     u8 cmd = 0;
+    switch (reg)
+    {
+        case CMD_GAIN_WRITE:   cmd = CMD_GAIN_READ; break; 
+        case CMD_OFFSET_WRITE: cmd = CMD_OFFSET_READ; break;		
+        case CMD_CONFIG_WRITE: cmd = CMD_CONFIG_READ; break; 
+    }
+
+    tmp =  readRegister(cmd);
+    tmp &= ~dat;
+    writeChar(reg);
+    writeLong(tmp);
+}
+
+void CS5530::writeChar(u8 dat) {
+ 
+    digitalWrite(_ss, LOW);
+    _spi->beginTransaction(_spiSettings);
+    SPI.transfer(dat & 0xFF); 
+    _spi->endTransaction();
+    digitalWrite(_ss, HIGH);
+}
+
+void CS5530::writeLong(u32 dat) {
+    int i;
+    u8 tmp;
+
+    for(i=3; i>=0; i--) {
+        tmp = (u8)( (dat >> (8*i)) & 0xff);
+        writeChar(tmp);
+    }
+}
+ 
+u32 CS5530::readRegister(u8 reg) {
+    u32 dat;
+    
+    writeChar(reg);
+    dat = readLong();
+    
+    return dat;
+}
+
+u32 CS5530::readLong(void)      {
+    int i;
+    u32 dat=0; 
+    u8 currntByte = 0;
+   
+    for(i=0; i<4; i++) {
+        dat    <<=    8;
+        dat    |= readChar();
+    }
+
+    return dat;
+}
+
+u8 CS5530::readChar(void)     {
+    u8 dat=0;
+      
+    digitalWrite(_ss, LOW);
+    _spi->beginTransaction(_spiSettings);
+    dat = SPI.transfer(CMD_NULL);
+    _spi->endTransaction();
+    digitalWrite(_ss, HIGH);
+      
+    return dat;
+}
+
+bool CS5530::isReady(void) {
+    //if(digitalRead(51) == 0) // Usar a porta SDI
+    if(0 == 0) // Usar a porta SDI
+    {
+        return true;
+    }
+
+    return false;
+}
+
+// Funções De Execução:
+
+u32 CS5530::readWeightsclae() { //Leitura do valor do ADC
+    u32 rec_data = 0;
+    
+    EAdStatus status;
+
+   if(isReady()==false)
+   {
+       status = E_AD_STATUS_BUSY;
+       Serial.println("Ocupado!");
+       return -2;
+   }
+   else
+   {    
+       rec_data = readRegister(CMD_NULL);
+       if((rec_data &  REG_DATA_OF) == 0)
+       {
+           rec_data &= 0xFFFFFF00;
+           
+           rec_data = rec_data >> 8;
+
+           status =  E_AD_STATUS_READY;
+           
+           //Serial.println("Pronto!");
+           return rec_data;
+       }
+       else
+       {
+             status =  E_AD_STATUS_OVERFLOW;
+             Serial.println("Overflow");
+             return -1;
+       }
+   }
+
+   return status;
+}
+
+u8 CS5530::calibrate(u8 calibrate_type, int cfg_reg, int setup_reg) { //função calibração
+    u32 calibrate_result;
+    int waste_time, i;
+    cfg_reg = (int)((calibrate_type % 2 == 1) ? (cfg_reg|REG_CONFIG_IS):(cfg_reg));
+    u8 cmd,read_reg;
+
+    writeRegister(CMD_CONFIG_WRITE, cfg_reg);
+    writeChar(cmd);
+
+    for(waste_time = WASTE_TIME; waste_time > 0; waste_time--);
+
+    calibrate_result = readRegister(read_reg);
+
+    /*Serial.print("Resultado da Calibracao: ");
+    Serial.printf("%x ", calibrate_result);
+    Serial.print("\n");*/
+
+    return calibrate_result;
+}
+
+u32 CS5530::twoComplement(u32 n) { //caso seja complemento de dois.
+    u32 negative = (n & (1UL << 23)) != 0;
+    u32 native_int;
+
+    if (negative)
+      native_int = n | ~((1UL << 24) - 1);
+    else
+      native_int = n;
+    return native_int;
+}
+
+u8 CS5530::convert(u8 convert_type, u8 setup_reg_no, u8 reg_no, int word_rate) { // serve para conversao caso complemento de dois
+    Serial.print("Preparando para a conversao...\n");
+    u32 final_result = 0;
+    int waste_time, i;
+    u32 cfg_reg =  (u32)REG_CONFIG_VRS;
+    int setup_reg = ((setup_reg_no % 2 == 1) ? ((word_rate) << 16) : word_rate);
+    u8 cmd;
+
+    switch (convert_type)
+    {
+        case SINGLE_CONVERSION: cmd = CMD_CONVERSION_SIGLE; break;
+        case CONTINUED_CONVERSION: cmd = CMD_CONVERSION_CONTINU; break; 
+    }
+
+    writeRegister(CMD_CONFIG_WRITE, cfg_reg);
+    void __nop();;//fazer if com nop;
+    writeChar(cmd);
+    Serial.print("Conversao Iniciada...\n");
+
+    void __nop();
+    u8 test = 0;
+    test = readChar();   // wastercycles
+    final_result = readLong();
+
+
+    Serial.print("Resultado Bruto:"); Serial.println(final_result);
+    //Serial.print("Resultado Bruto:"); Serial.println(final_result);
+
+    final_result = twoComplement(final_result);
+    Serial.print("Resultado Final:"); Serial.println(final_result);
+    //Serial.print("Resultado Final:"); Serial.println(final_result);
+
+    final_result = final_result * 500 / 0x7fffff;
+    Serial.print("Resultado Final:"); Serial.println(final_result);
+
+    return 1;
+}
+
+
+/*
+
+Operação: 
+1° Sincronizar o SPI
+2° Resetar o registrador de comando
+3° Definir os parametros de operação; Ex: Bipolar, continua, Complemento de dois, etc.
+4° Calibrar;
+5° verificar o resultado da calibração;
+6° Iniciar a conversão;
+7° ler o resultado da conversão.
+8° plotar o resultado da conversão
+
+*/
---
+++ src/CS5530.h
@@ -0,0 +1,256 @@
+#ifndef CS5530_H
+#define CS5530_H
+#include <SPI.h>
+
+/*
+=============================================================
+                        Timing Table
+=============================================================
+                           Start-up
+
+Oscillator Start-up Time XTAL = 4.9152 MHz
+=============================================================
+                     Serial Port Timing
+
+Serial Clock Frequency SCLK               0  -  2 MHz
+=============================================================
+                        Serial Clock
+
+Pulse Width High                          t1    250ns
+Pulse Width Low                           t2    250ns
+=============================================================
+                     SDI Write Timing
+
+CS Enable to Valid Latch Clock            t3    50 ns
+Data Set-up Time prior to SCLK rising     t4    50 ns
+Data Hold Time After SCLK Rising          t5    100 ns
+SCLK Falling Prior to CS Disable          t6    100 ns
+=============================================================
+                     SDO Read Timing
+
+CS to Data Valid                          t7    150 ns (MAX)
+SCLK Falling to New Data Bit              t8    150 ns (MAX)
+CS Rising to SDO Hi-Z                     t9    150 ns (MAX)
+=============================================================
+
+*Refer to DS742F3, page 9;
+
+*/
+
+//Espelhamento de dados
+
+#define i32 long int
+#define i16 int
+#define i8  char
+#define u32 unsigned long int
+#define u16 unsigned int
+#define u8  unsigned char
+
+//===================================================================================================================================================//
+
+#define RESET_TIME               1200000       // Tempo de Reset - 18 ciclos ao menos (verificar o tempo de cada ciclo em acordo com o clock setado);
+#define WASTE_TIME               90000000      // Tempo para a conversão (40 ciclos de clock) ?
+
+//===================================================================================================================================================//
+
+#define SELF_OFFSET              1              // Manda 0000 1001
+#define SELF_GAIN                2              // Manda 0000 1001
+#define SYSTEM_OFFSET            3              // Manda 0000 1001
+#define SYSTEM_GAIN              4              // Manda 0000 1001
+
+#define SINGLE_CONVERSION        1              // Manda 0000 1001
+#define CONTINUED_CONVERSION     2              // Manda 0000 1001
+
+//===================================================================================================================================================//
+
+//Espelho para resultados de operações
+
+#define PASS                     0x00           // Manda 0000 0000
+#define FAIL                     0xFF           // Manda 1111 1111
+#define TRUE                     1              // Manda 1
+#define FALSE                    0              // Manda 0
+
+//===================================================================================================================================================//
+
+#define NOTHING                  0              // Manda 0
+
+#define CS5530_READ_DATA_TIMEOUT 20000          // Tempo Limite para Ler dados (20s)
+
+#define TIMEOUTERR               0xFF           // Manda 1111 1111
+ 
+#define SUCCESSFUL               0x00           // Manda 0000 0000
+
+#define CMD_STOP_CONT_CONV       0xFF           // Manda 1111 1111
+
+#define CAL_TIMEOUT_LIMIT        4000           // Tempo Limite para Calibrar (4s)
+
+#define READ_CONV_RESULT         0x00           // Manda 0000 0000
+
+//===================================================================================================================================================//
+
+#define DATA_VALID               0              // Caso os dados sejam validos, responde o booleano 0;
+ 
+#define ERR_AD_BUSY              1              // Conversor ocupado, retorna 1;
+ 
+#define ERR_AD_OVER_FLOW         2              // Overflow (estoura de pilha), retorna 2;
+ 
+#define ERR_FILTER_ONGOING       3              // Filtro em andamento retorna 3;
+
+//===================================================================================================================================================//
+// *2.3.10 page 19* Configuration Register Descriptions Where 1UL is an unsigned long int, with a value of 1 represented at the bit level as:
+// 00000000000000000000000000000001. Is possible to move 0's by the same process, declarating 0UL;
+//===================================================================================================================================================//
+
+#define REG_CONFIG_PSS           1UL << 31     //Power Save Select         0 Standby Mode (Oscillator active, allows quick power-up). 1 Sleep Mode (Oscillator inactive).
+#define REG_CONFIG_PDM           1UL << 30     //Power Down Mode           0 Normal Mode.       1 Activate the power save select mode.
+#define REG_CONFIG_RS            1UL << 29     //Reset System              0 Normal Operation.  1 Activate a Reset cycle. See System Reset Sequence in the datasheet text.
+#define REG_CONFIG_RV            1UL << 28     //Reset Valid               0 Normal Operation.  1 System was reset. This bit is read only. Bit is cleared to logic zero after the configuration register is read.
+#define REG_CONFIG_IS            1UL << 27     //Input Short               0 Normal Input.      1 All signal input pairs for each channel are disconnected from the pins and shorted internally.
+#define REG_CONFIG_VRS           0UL << 25     //Voltage Reference Select  0 2.5 V < VREF ≤ [(VA+) - (VA-)]. 1 1 V ≤ VREF ≤ 2.5V
+#define REG_CONFIG_A1            1UL << 24     //Output Latch bit          00 A1 = 0, A0 = 0 // 10 A1 = 1, A0 = 0
+#define REG_CONFIG_A0            1UL << 23     //Output Latch bit          01 A1 = 0, A0 = 1 // 11 A1 = 1, A0 = 1
+#define REG_CONFIG_FRS           1UL << 19     //Filter Rate Select        0 Use the default output word rates. 1 Scale all output word rates and their corresponding filter characteristics by a factor of 5/6.
+#define REG_CONFIG_OCD           1UL << 9      //Open Circuit Detect Bit   0 Normal mode. 1 Activate current source (Used for thermocouple).
+
+//===================================================================================================================================================//
+
+//CS5532 Gain settings
+
+#define GAINX1                   1UL  << 24    // Gain at 1x
+#define GAINX2                   2UL  << 24    // Gain at 2x
+#define GAINX4                   4UL  << 24    // Gain at 4x
+#define GAINX8                   8UL  << 24    // Gain at 8x
+#define GAINX16                  16UL << 24    // Gain at 16x
+#define GAINX32                  32UL << 24    // Gain at 32x
+#define GAINX64                  64UL << 24    // Gain at 64x
+
+//===================================================================================================================================================//
+
+//Word Rate when FSR=1
+
+#define WORD_RATE_100SPS         0x0 << 11     // Manda 0000 e desloca 11 posicoes para a esquerda
+#define WORD_RATE_50SPS          0x1 << 11     // Manda 0001 e desloca 11 posicoes para a esquerda
+#define WORD_RATE_25SPS          0x2 << 11     // Manda 0010 e desloca 11 posicoes para a esquerda
+#define WORD_RATE_12P5SPS        0x3 << 11     // Manda 0011 e desloca 11 posicoes para a esquerda
+#define WORD_RATE_6P25SPS        0x4 << 11     // Manda 0100 e desloca 11 posicoes para a esquerda
+#define WORD_RATE_3200SPS        0x8 << 11     // Manda 1000 e desloca 11 posicoes para a esquerda
+#define WORD_RATE_1600SPS        0x9 << 11     // Manda 1001 e desloca 11 posicoes para a esquerda
+#define WORD_RATE_800SPS         0xA << 11     // Manda 1010 e desloca 11 posicoes para a esquerda
+#define WORD_RATE_400SPS         0xB << 11     // Manda 1011 e desloca 11 posicoes para a esquerda
+#define WORD_RATE_200SPS         0xC << 11     // Manda 1100 e desloca 11 posicoes para a esquerda
+
+//===================================================================================================================================================//
+/*
+
+The listed Word Rates are for continuous conversion mode using a 4.9152 MHz clock. All word rates will
+scale linearly with the clock frequency used. The very first conversion using continuous conversion mode
+will last longer, as will conversions done with the single conversion mode. See the section on Performing
+Conversions and Tables 1 and 2 for more details.
+
+Word Rate Table
+
+Bit      WR(FRS = 0) WR(FRS = 1)
+
+0000     120 Sps     100 Sps
+0001     60 Sps      50 Sps
+0010     30 Sps      25 Sps
+0011     15 Sps      12.5 Sps
+0100     7.5 Sps     6.25 Sps
+1000     3840 Sps    3200 Sps
+1001     1920 Sps    1600 Sps
+1010     960 Sps     800 Sps
+1011     480 Sps     400 Sps
+1100     240 Sps     200 Sps
+*/
+//===================================================================================================================================================//
+
+//Unipolar / Bipolar
+#define CS5530_UNIPOLAR          1UL << 10     // Manda 1 e desloca 10 para a esquerda. 1 Select Unipolar mode.
+#define CS5530_BIPOLAR           0UL << 10     // Manda 0 e desloca 10 para a esquerda. 0 Select Bipolar mode.
+#define REG_DATA_OF              1UL << 3      // Manda 1 e desloca 03 posicoes para a esquerda
+
+//===================================================================================================================================================//
+
+#define CMD_STOP_CONT_CONV       0xFF          // Manda 1111 1111
+
+//===================================================================================================================================================//
+
+#define CS5530_DEFAULT_SPI           SPI           // Define que o SPI é o SPI da biblioteca arduino SPI.
+#define CS5530_DEFAULT_SPI_FREQUENCY 20000000      //(1E8480 == 2MHz) (Original 8E6 == 2278 (Hz?) )
+#define CS5530_DEFAULT_SS_PIN        53            // Pino CS do arduino MEGA 2560.
+
+/* 
+No arduino MEGA 2560, os pinos SPI são definidos como:
+
+    CS/SS == 53
+    SCK   == 52
+    SDO   == 51
+    SDI   == 50f 
+    
+*/
+
+//===================================================================================================================================================//
+
+//Command Register (CS5530_COMM_REG)
+
+#define CMD_OFFSET_READ         0x09 // Manda 0000 1001
+#define CMD_OFFSET_WRITE        0x01 // Manda 0000 0001
+#define CMD_GAIN_READ           0x0A // Manda 0000 1010
+#define CMD_GAIN_WRITE          0x02 // Manda 0000 0010
+#define CMD_CONFIG_READ         0x0B // Manda 0000 1011
+#define CMD_CONFIG_WRITE        0x03 // Manda 0000 0011
+
+#define CMD_CONVERSION_SIGLE    0x80 // Manda 1000 0000
+#define CMD_CONVERSION_CONTINU  0xC0 // Manda 1100 0000
+#define CMD_SYS_OFFSET_CALI     0x85 // Manda 1000 0101
+#define CMD_SYS_GAIN_CALI       0x86 // Manda 1000 0110
+#define CMD_SYNC1               0xFF // Manda 1111 1111   //Function: Part of the serial port re-initialization sequence.
+#define CMD_SYNC0               0xFE // Manda 1111 1110   //Function: End of the serial port re-initialization sequence.
+#define CMD_NULL                0x00 // Manda 0000 0000   //Function: This command is used to clear a port flag and keep the converter in the continuous conversion mode.
+
+//===================================================================================================================================================//
+//Sinalização de status do conversor;
+
+    enum EAdStatus {
+       E_AD_STATUS_BUSY,
+       E_AD_STATUS_READY,
+       E_AD_STATUS_OVERFLOW
+    };
+
+//===================================================================================================================================================//
+class CS5530 {
+public:
+    CS5530();
+    int begin();                                 // Inicia o CS5530
+    void setPin (int ss = CS5530_DEFAULT_SS_PIN); // Função SETPIN do Controlador, caso necessário usar para definir o Pino em outro Arduino
+    void setSPI (SPIClass &spi);                  // Inicia o SPI do controlador
+    void setSPIFrequency (u32);                   // Define a Frequencia do SPI (CS5530 suporta no maximo 2Mhz);
+    u32 twoComplement (u32);                      // Define como Complemento de 2 a conversão
+    u8 readChar (void);                           // Inicia a Leitura de um vetor de caracteres (registrador) como char
+    u32 readLong (void);                          // Inicia a Leitura de um vetor de inteiros (registrador) como long
+    u32 readRegister (u8);                        // Inicia a Leitura de um registrador
+    u8 convert (u8, u8, u8, int);                 // Define a conversao (onde armazenara)
+    u8 calibrate (u8, int, int);                  // Define a calibração como u32
+    void writeLong (u32);                         // Define a escrita de uma cadeia de inteiros do tipo long, 32 bits
+    void writeChar (u8);                          // Escreve uma cadeia de inteiros de 8 bits
+    void writeRegister (u8, u32);                 // Escreve no registrador 8 ou 32 bits como cadeia de inteiros
+    void setBit (u8, u32);                        // Define os Bits
+    void resetBit (u8, u32);                      // Redefine os Bits
+    bool isReady (void);                          // Define que o conversor está pronto
+    bool reset (void);                            // Função de RESET
+    u32 readWeightsclae ();                       // Define a chamada de Leitura da conversao
+    //===================================================================================================================================================//
+
+private:
+    SPISettings _spiSettings;
+    SPIClass *_spi;
+    int _ss;
+};
+
+//===================================================================================================================================================//
+
+#endif
+
+
+
+
